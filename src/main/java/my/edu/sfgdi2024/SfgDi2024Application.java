250d559be5b03c7a9c86157954e764f0220c8570
claud <crobinson@tec.mx>
2024-04-12 16:29:32 -0600

    Initial Spring setup

---
+++ src/main/java/my/edu/sfgdi2024/SfgDi2024Application.java
@@ -0,0 +1,13 @@
+package my.edu.sfgdi2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgDi2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgDi2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/my/edu/sfgdi2024/SfgDi2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package my.edu.sfgdi2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDi2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
